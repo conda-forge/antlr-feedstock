872ada8f32fb32cdc6833566d1d9797ea19710ea
ocefpaf <ocefpaf@gmail.com>
2016-08-08 12:33:40 -0300

    No need for the python support for NCO

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ $(uname) == Darwin ]; then
+    export CC=clang
+    export CXX=clang++
+    export MACOSX_DEPLOYMENT_TARGET="10.9"
+    export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
+    export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
+fi
+
 ./configure --prefix=$PREFIX \
             --enable-cxx \
-            --enable-python \
+            --disable-python \
             --enable-csharp \
             --disable-java \
 
 make
+# No make check :-(
 make install
