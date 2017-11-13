a282343ae3d16b0f2ded2c0a61100529d3802d0b
Filipe Fernandes <ocefpaf@gmail.com>
2017-11-13 14:20:55 -0200

    copy the proj file to the right place

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,8 +4,9 @@ if "%ARCH%"=="32" (
     set PLATFORM=x64
 )
 
-copy %RECIPE_DIR%/antlr.vcxproj .
+
+copy %RECIPE_DIR%\antlr.vcxproj lib\cpp\
 if errorlevel 1 exit 1
 
-msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release antlr.vcxproj
+msbuild.exe /p:Platform=%PLATFORM% /p:Configuration=Release lib\cpp\antlr.vcxproj
 if errorlevel 1 exit 1
