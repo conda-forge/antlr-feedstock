223567f11475c0a57dcb97978467ad4fc95898ab
Nicholas Bollweg <nbollweg@continuum.io>
2017-12-10 16:22:17 -0500

    clean up silly python wrapper writer with better escaping

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,8 @@
 set MAVEN_OPTS="-Xmx1G"
 
-REM cmd.exe /c mvn --projects=tool clean || echo ""
+cmd.exe /c mvn --projects=tool clean || echo ""
 cmd.exe /c mvn --projects=tool -DskipTests install || echo ""
 
 copy "%SRC_DIR%\tool\target\antlr4-%PKG_VERSION%-complete.jar" "%LIBRARY_LIB%\"
-
-"%PYTHON%" "%RECIPE_DIR%\make_wrapper.py"
+echo java -Xmx500M -cp %LIBRARY_LIB%\\antlr4-%PKG_VERSION%-complete.jar;%%CLASSPATH%% org.antlr.v4.Tool %* > %LIBRARY_BIN%\antlr4.cmd
+echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\antlr4.cmd
--- recipe/build.sh
+++ recipe/build.sh
@@ -6,5 +6,6 @@ mvn --projects=tool -DskipTests install
 
 cp "${SRC_DIR}/tool/target/antlr4-${PKG_VERSION}-complete.jar" "${PREFIX}/lib/"
 
-"${PYTHON}" "${RECIPE_DIR}/make_wrapper.py"
+echo '#!/bin/bash' > $PREFIX/bin/antlr4
+echo 'java -Xmx500M -cp "'$PREFIX'/lib/antlr4-'$PKG_VERSION'-complete.jar:$CLASSPATH" org.antlr.v4.Tool "$@"' >> $PREFIX/bin/antlr4
 chmod +x "${PREFIX}/bin/antlr4"
