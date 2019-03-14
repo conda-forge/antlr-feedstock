96a39fa3b07b1ec360057351459fa6770a5604ad
Carlo <carlodri@users.noreply.github.com>
2019-03-14 12:13:05 +0100

    escape percent

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -4,5 +4,5 @@ cmd.exe /c mvn --projects=tool clean || echo ""
 cmd.exe /c mvn --projects=tool -DskipTests install || echo ""
 
 copy "%SRC_DIR%\tool\target\antlr4-%PKG_VERSION%-complete.jar" "%LIBRARY_LIB%\"
-echo java -Xmx500M -cp %LIBRARY_LIB%\\antlr4-%PKG_VERSION%-complete.jar;%%CLASSPATH%% org.antlr.v4.Tool %* > %LIBRARY_BIN%\antlr4.cmd
+echo java -Xmx500M -cp %LIBRARY_LIB%\\antlr4-%PKG_VERSION%-complete.jar;%%CLASSPATH%% org.antlr.v4.Tool %%* > %LIBRARY_BIN%\antlr4.cmd
 echo IF %%ERRORLEVEL%% NEQ 0 EXIT /B %%ERRORLEVEL%% >> %LIBRARY_BIN%\antlr4.cmd
