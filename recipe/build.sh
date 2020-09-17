14ae47259b1f4d0c7aebadcfe147c8239323679c
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2020-09-17 11:43:44 +0200

    Add grun script for grammar testing

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,3 +16,7 @@ cp "${SRC_DIR}/tool/target/antlr4-${PKG_VERSION}-complete.jar" "${PREFIX}/lib/"
 echo '#!/bin/bash' > $PREFIX/bin/antlr4
 echo 'java -Xmx500M -cp "'$PREFIX'/lib/antlr4-'$PKG_VERSION'-complete.jar:$CLASSPATH" org.antlr.v4.Tool "$@"' >> $PREFIX/bin/antlr4
 chmod +x "${PREFIX}/bin/antlr4"
+
+echo '#!/bin/bash' > $PREFIX/bin/grun
+echo 'java -Xmx500M -cp "'$PREFIX'/lib/antlr4-'$PKG_VERSION'-complete.jar:$CLASSPATH" org.antlr.v4.gui.TestRig "$@"' >> $PREFIX/bin/grun
+chmod +x "${PREFIX}/bin/grun"
