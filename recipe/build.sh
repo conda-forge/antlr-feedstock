e7fff6be816b25c0081d14de6677220fc13436a2
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-12-20 22:16:25 +0100

    Disable csharp for osx

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,18 @@ set -xe
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./scripts/
 
+if [ $(uname) == Darwin ]; then
+    CSHARP=--disable-csharp
+else
+    CSHARP=--enable-csharp
+fi
+
 ./configure --prefix=$PREFIX \
             --host="${HOST}" \
             --build="${BUILD}" \
             --enable-cxx \
             --disable-python \
-            --enable-csharp \
+            ${CSHARP} \
             --disable-java \
 
 make
