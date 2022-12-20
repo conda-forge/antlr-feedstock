970833dc070ed4274c0c7365aab217a5c76daf8e
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2022-12-20 21:03:15 +0100

    Get updated scripts into the right place

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* .
+cp $BUILD_PREFIX/share/gnuconfig/config.* ./scripts/
 
 ./configure --prefix=$PREFIX \
             --host="${HOST}" \
