9eadf905a7ee636d517b3e168453c6447c40dcd5
Xylar Asay-Davis <xylarstorm@gmail.com>
2025-09-04 20:11:24 +0200

    Fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -13,9 +13,22 @@ if defined SDKVER (
   if "%SDKVER:~-1%"=="\" set "SDKVER=%SDKVER:~0,-1%"
 ) else set "SDKVER=10.0"
 
-copy "%RECIPE_DIR%\antlr.vcxproj" "lib\cpp\" || exit 1
+rem --- Sanity check: the ANTLR2 runtime sources must be here:
+echo Listing %SRC_DIR%\lib\cpp\src
+dir /b "%SRC_DIR%\lib\cpp\src" || echo (dir failed)
 
-msbuild.exe lib\cpp\antlr.vcxproj ^
+if not exist "%SRC_DIR%\lib\cpp\src\ANTLRUtil.cpp" (
+  echo ERROR: Missing runtime sources under %SRC_DIR%\lib\cpp\src
+  echo Showing what exists under %SRC_DIR%\lib\cpp:
+  dir /s /b "%SRC_DIR%\lib\cpp"
+  exit /b 1
+)
+
+copy "%RECIPE_DIR%\antlr.vcxproj" "%SRC_DIR%\lib\cpp\" || exit 1
+
+pushd "%SRC_DIR%\lib\cpp"
+
+msbuild.exe "antlr.vcxproj" ^
   /m ^
   /p:Platform=%PLATFORM% ^
   /p:Configuration=Release ^
@@ -23,12 +36,12 @@ msbuild.exe lib\cpp\antlr.vcxproj ^
   /p:WindowsTargetPlatformVersion=%SDKVER% ^
   /p:PreferredToolArchitecture=x64
 if errorlevel 1 exit 1
+popd
 
 if not exist "%LIBRARY_LIB%" mkdir "%LIBRARY_LIB%"
 if not exist "%LIBRARY_INC%\antlr" mkdir "%LIBRARY_INC%\antlr"
 
-copy /y "lib\cpp\build\%PLATFORM%\Release\antlr.lib" "%LIBRARY_LIB%\antlr.lib" || exit 1
-copy /y "%SRC_DIR%\lib\cpp\antlr\*.h"   "%LIBRARY_INC%\antlr\" || exit 1
+copy /y "%SRC_DIR%\lib\cpp\build\%PLATFORM%\Release\antlr.lib" "%LIBRARY_LIB%\antlr.lib" || exit 1
 copy /y "%SRC_DIR%\lib\cpp\antlr\*.hpp" "%LIBRARY_INC%\antlr\" || exit 1
 
 endlocal
